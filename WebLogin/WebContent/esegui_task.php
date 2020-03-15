3f943466beb0f53ae3839991f2b3cfd82edf3724
Matteo Bassi <matteo.bassi@tin.it>
2020-03-15 16:59:19 +0100

    Aggiunta pulsante reset sul form di inserimento
    Aggiunto pulsante di reset in esegui_task.php per pulirer i campi del
    form

--- WebLogin/WebContent/esegui_task.php
+++ WebLogin/WebContent/esegui_task.php
@@ -6,13 +6,15 @@ if (IsLogged()) {
     echo "<p> Buongiorno user <b>";
     echo $_SESSION["utente"];
     echo "</b></p>";
-    // una volta controllata la variabile di sessione, la elimino per evitare che rimanga memorizzata
-    // più a lungo di quello che serve.
+    echo "<h1> form di inserimento nuovo user & psw </h1>";
     echo "<form name='moduloinserimento' action='inseriscinewuser.php' method='post'>";
     echo "<p> user: <input name='newuser' type='text'> </p>";
     echo "<p> psw: <input name='newpsw' type='password'> </p>";
-    echo "<p> <input name='bottoneinvio' value='invio' type='submit'> </p>";
+    echo "<p> <input name='bottoneinvio' value='invio' type='submit'>";
+    echo "<input value='Reimposta' type='reset'> </p>";
     echo "</form>";
+    // una volta controllata la variabile di sessione, la elimino per evitare che rimanga memorizzata
+    // più a lungo di quello che serve.
     unset($_SESSION["utente"]);
  } else {
     echo "Login non effettuato";
